94055463bca78e93a26b8eb6e65c099563a977f4
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-12-02 12:16:29 +0700

    Update init files

---
+++ metaperceptron/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 15:23, 10/08/2023 ----------%
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+__version__ = "1.0.0"
+
+from metaperceptron.helpers.preprocessor import Data, DataTransformer
+from metaperceptron.core.standard_mlp import MlpRegressor, MlpClassifier
+# from metaperceptron.core.mha_mlp import MhaMlpRegressor, MhaFlnnClassifier
