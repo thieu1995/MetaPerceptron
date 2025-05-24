09f93b163ff705c970d938e1977a48bd2c70aaab
anh9895 <honganh090895@gmail.com>
2025-05-24 09:35:08 +0700

    Refactor gradient_mlp module

--- metaperceptron/core/gradient_mlp.py
+++ metaperceptron/core/gradient_mlp.py
@@ -40,7 +40,7 @@ class MlpClassifier(BaseStandardMlp, ClassifierMixin):
     optim : str, default="Adam"
         Optimizer to use, selected from the supported optimizers.
 
-    optim_paras : dict, default=None
+    optim_params : dict, default=None
         Parameters for the optimizer, such as learning rate, beta values, etc.
 
     early_stopping : bool, default=True
@@ -60,19 +60,23 @@ class MlpClassifier(BaseStandardMlp, ClassifierMixin):
 
     verbose : bool, default=True
         If True, prints training progress and validation loss during training.
+
+    device : str, default=None
+        Device to use for training (e.g., "cpu" or "gpu"). If None, uses the default device.
     """
 
     def __init__(self, hidden_layers=(100,), act_names="ReLU", dropout_rates=0.2, act_output=None,
-                 epochs=1000, batch_size=16, optim="Adam", optim_paras=None,
+                 epochs=1000, batch_size=16, optim="Adam", optim_params=None,
                  early_stopping=True, n_patience=10, epsilon=0.001, valid_rate=0.1,
-                 seed=42, verbose=True):
+                 seed=42, verbose=True, device=None):
         # Call superclass initializer with the specified parameters.
         super().__init__(hidden_layers, act_names, dropout_rates, act_output,
-                         epochs, batch_size, optim, optim_paras,
-                         early_stopping, n_patience, epsilon, valid_rate, seed, verbose)
+                         epochs, batch_size, optim, optim_params,
+                         early_stopping, n_patience, epsilon, valid_rate,
+                         seed, verbose, device)
         self.classes_ = None
 
-    def process_data(self, X, y, **kwargs):
+    def _process_data(self, X, y, **kwargs):
         """
         Prepares and processes data for training, including optional splitting into validation data.
 
@@ -102,20 +106,20 @@ class MlpClassifier(BaseStandardMlp, ClassifierMixin):
                 raise ValueError("Validation rate must be between 0 and 1.")
 
         # Convert data to tensors and set up DataLoader
-        X_tensor = torch.tensor(X, dtype=torch.float32)
-        y_tensor = torch.tensor(y, dtype=torch.long)
+        X_tensor = torch.tensor(X, dtype=torch.float32).to(self.device)
+        y_tensor = torch.tensor(y, dtype=torch.long).to(self.device)
         if self.task == "binary_classification":
             y_tensor = torch.tensor(y, dtype=torch.float32)
-            y_tensor = torch.unsqueeze(y_tensor, 1)
+            y_tensor = torch.unsqueeze(y_tensor, 1).to(self.device)
 
         train_loader = DataLoader(TensorDataset(X_tensor, y_tensor), batch_size=self.batch_size, shuffle=True)
 
         if self.valid_mode:
-            X_valid_tensor = torch.tensor(X_valid, dtype=torch.float32)
-            y_valid_tensor = torch.tensor(y_valid, dtype=torch.long)
+            X_valid_tensor = torch.tensor(X_valid, dtype=torch.float32).to(self.device)
+            y_valid_tensor = torch.tensor(y_valid, dtype=torch.long).to(self.device)
             if self.task == "binary_classification":
                 y_valid_tensor = torch.tensor(y_valid, dtype=torch.float32)
-                y_valid_tensor = torch.unsqueeze(y_valid_tensor, 1)
+                y_valid_tensor = torch.unsqueeze(y_valid_tensor, 1).to(self.device)
 
         return train_loader, X_valid_tensor, y_valid_tensor
 
@@ -151,7 +155,7 @@ class MlpClassifier(BaseStandardMlp, ClassifierMixin):
             self.size_output = len(self.classes_)
 
         # Process data for training and validation
-        data = self.process_data(X, y, **kwargs)
+        data = self._process_data(X, y, **kwargs)
 
         # Build the model architecture
         self.build_model()
@@ -175,7 +179,7 @@ class MlpClassifier(BaseStandardMlp, ClassifierMixin):
         numpy.ndarray
             Predicted class labels for each sample.
         """
-        X_tensor = torch.tensor(X, dtype=torch.float32)
+        X_tensor = torch.tensor(X, dtype=torch.float32).to(self.device)
         self.network.eval()
         with torch.no_grad():
             output = self.network(X_tensor)  # Get model predictions
@@ -183,7 +187,7 @@ class MlpClassifier(BaseStandardMlp, ClassifierMixin):
                 _, predicted = torch.max(output, 1)
             else:  # Binary classification
                 predicted = (output > 0.5).int().squeeze()
-        return predicted.numpy()
+        return predicted.cpu().numpy()
 
     def score(self, X, y):
         """
@@ -219,7 +223,7 @@ class MlpClassifier(BaseStandardMlp, ClassifierMixin):
         numpy.ndarray
             Probability predictions for each class.
         """
-        X_tensor = torch.tensor(X, dtype=torch.float32)
+        X_tensor = torch.tensor(X, dtype=torch.float32).to(self.device)
         if self.task not in ["classification", "binary_classification"]:
             raise ValueError("predict_proba is only available for classification tasks.")
 
@@ -227,7 +231,7 @@ class MlpClassifier(BaseStandardMlp, ClassifierMixin):
         with torch.no_grad():
             probs = self.network.forward(X_tensor)  # Forward pass to get probability estimates
 
-        return probs.numpy()  # Return as numpy array
+        return probs.cpu().numpy()  # Return as numpy array
 
     def evaluate(self, y_true, y_pred, list_metrics=("AS", "RS")):
         """
@@ -249,7 +253,7 @@ class MlpClassifier(BaseStandardMlp, ClassifierMixin):
         dict
             Dictionary with results for the specified metrics.
         """
-        return self._BaseMlp__evaluate_cls(y_true, y_pred, list_metrics)
+        return self._evaluate_cls(y_true=y_true, y_pred=y_pred, list_metrics=list_metrics)
 
 
 class MlpRegressor(BaseStandardMlp, RegressorMixin):
@@ -272,7 +276,7 @@ class MlpRegressor(BaseStandardMlp, RegressorMixin):
         Size of the mini-batch during training (default is 16).
     optim : str, optional
         Optimization algorithm (default is "Adam").
-    optim_paras : dict, optional
+    optim_params : dict, optional
         Additional parameters for the optimizer (default is None).
     early_stopping : bool, optional
         Flag to enable early stopping during training (default is True).
@@ -286,17 +290,20 @@ class MlpRegressor(BaseStandardMlp, RegressorMixin):
         Random seed for reproducibility (default is 42).
     verbose : bool, optional
         Flag to enable verbose output during training (default is True).
+    device : str, optional
+        Device to use for training (default is None, which uses the default device).
     """
 
     def __init__(self, hidden_layers=(100,), act_names="ELU", dropout_rates=0.2, act_output=None,
-                 epochs=1000, batch_size=16, optim="Adam", optim_paras=None,
+                 epochs=1000, batch_size=16, optim="Adam", optim_params=None,
                  early_stopping=True, n_patience=10, epsilon=0.001, valid_rate=0.1,
-                 seed=42, verbose=True):
+                 seed=42, verbose=True, device=None):
         super().__init__(hidden_layers, act_names, dropout_rates, act_output,
-                         epochs, batch_size, optim, optim_paras,
-                         early_stopping, n_patience, epsilon, valid_rate, seed, verbose)
+                         epochs, batch_size, optim, optim_params,
+                         early_stopping, n_patience, epsilon, valid_rate,
+                         seed, verbose, device)
 
-    def process_data(self, X, y, **kwargs):
+    def _process_data(self, X, y, **kwargs):
         """
         Prepares the input data for training and validation by converting it to tensors
         and creating DataLoaders for batch processing.
@@ -326,8 +333,8 @@ class MlpRegressor(BaseStandardMlp, RegressorMixin):
                 raise ValueError("Validation rate must be between 0 and 1.")
 
         # Convert training data to tensors
-        X_tensor = torch.tensor(X, dtype=torch.float32)
-        y_tensor = torch.tensor(y, dtype=torch.float32)
+        X_tensor = torch.tensor(X, dtype=torch.float32).to(self.device)
+        y_tensor = torch.tensor(y, dtype=torch.float32).to(self.device)
 
         # Ensure the target tensor has correct dimensions
         if y_tensor.ndim == 1:
@@ -338,8 +345,8 @@ class MlpRegressor(BaseStandardMlp, RegressorMixin):
 
         # Process validation data if in validation mode
         if self.valid_mode:
-            X_valid_tensor = torch.tensor(X_valid, dtype=torch.float32)
-            y_valid_tensor = torch.tensor(y_valid, dtype=torch.float32)
+            X_valid_tensor = torch.tensor(X_valid, dtype=torch.float32).to(self.device)
+            y_valid_tensor = torch.tensor(y_valid, dtype=torch.float32).to(self.device)
             if y_valid_tensor.ndim == 1:
                 y_valid_tensor = y_valid_tensor.unsqueeze(1)
 
@@ -373,7 +380,7 @@ class MlpRegressor(BaseStandardMlp, RegressorMixin):
             self.size_output = y.shape[1]
 
         # Process data to prepare DataLoader and tensors
-        data = self.process_data(X, y, **kwargs)
+        data = self._process_data(X, y, **kwargs)
 
         # Build the MLP model
         self.build_model()
@@ -398,11 +405,11 @@ class MlpRegressor(BaseStandardMlp, RegressorMixin):
             Predicted values for the input features.
         """
         # Convert input features to tensor
-        X_tensor = torch.tensor(X, dtype=torch.float32)
+        X_tensor = torch.tensor(X, dtype=torch.float32).to(self.device)
         self.network.eval()  # Set model to evaluation mode
         with torch.no_grad():
             predicted = self.network(X_tensor)  # Forward pass to get predictions
-        return predicted.numpy()  # Convert predictions to numpy array
+        return predicted.cpu().numpy()  # Convert predictions to numpy array
 
     def score(self, X, y):
         """
@@ -441,4 +448,4 @@ class MlpRegressor(BaseStandardMlp, RegressorMixin):
         results : dict
             A dictionary containing the results of the specified metrics.
         """
-        return self._BaseMlp__evaluate_reg(y_true, y_pred, list_metrics)  # Call the evaluation method
+        return self._evaluate_reg(y_true, y_pred, list_metrics)  # Call the evaluation method
